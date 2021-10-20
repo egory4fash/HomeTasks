8ec053e38c162280a0f5327d0f03cd2fd5db992c
egory4fash <egormaksul@gmail.com>
2021-10-20 20:58:18 +0300

    HW

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from "./Message.module.css";
+
+
+type MessagePropsType = {
+    avatar: string,
+    message: string,
+    name: string,
+    time: string
+}
+
+function Message(props: MessagePropsType) {
+    return (
+        <div>
+            <div className={classes.wrap}>
+
+                <img src={props.avatar}/>
+
+                <div className={classes.tail} />
+                <div className={classes.message}>
+                    <div className={classes.name}>{props.name}</div>
+                    <div className={classes.msg}>{props.message}</div>
+                    <div className={classes.time}>{props.time}</div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+};
+
+export default Message;
