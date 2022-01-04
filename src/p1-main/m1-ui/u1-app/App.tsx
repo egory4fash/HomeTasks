01d8c25f6f59767eae94c6dfd251cdeed4dbffcd
egory4fash <egormaksul@gmail.com>
2022-01-04 22:38:09 +0300

    HW5 broken links

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -2,18 +2,19 @@ import React from 'react'
 import s from './App.module.css'
 import HW5 from "../../../p2-homeworks/h5/HW5";
 import Collapsible from 'react-collapsible'
-
+import {NavLink,HashRouter} from 'react-router-dom'
 
 function App() {
     return (
-
+<HashRouter>
         <div className={s.App}>
             <div>react homeworks:</div>
             <Collapsible className={s.Collapse} trigger={'PUSH ME'}>
                 <div className={s.Links}>
-                    <p> link1  </p>
-                    <p> link2  </p>
-                    <p> link3  </p>
+                    <NavLink to = '/../p2-homeworks/h5/pages/PreJunior'>PreJunor</NavLink>
+                    <NavLink to = '../../../p2-homeworks/h5/pages/PreJunior'>PreJunor</NavLink>
+                    <NavLink to = '../../../p2-homeworks/h5/pages/PreJunior'>PreJunor</NavLink>
+
                 </div>
             </Collapsible>
             {/*<HW1/>*/}
@@ -24,7 +25,7 @@ function App() {
 
 
         </div>
-
+</HashRouter>
     )
 }
 
