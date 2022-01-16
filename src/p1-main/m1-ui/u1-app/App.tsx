dee7d34e9a7d4db69bcb66b6b4922da3c1638c2c
egory4fash <egormaksul@gmail.com>
2022-01-16 12:14:17 +0300

    HW5 done

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -2,32 +2,46 @@ import React from 'react'
 import s from './App.module.css'
 import HW5 from "../../../p2-homeworks/h5/HW5";
 import Collapsible from 'react-collapsible'
-import {NavLink,HashRouter} from 'react-router-dom'
+import {NavLink, HashRouter} from 'react-router-dom'
 import {PATH} from "../../../p2-homeworks/h5/Routes";
 
+
 function App() {
+
+
     return (
-<HashRouter>
-        <div className={s.App}>
-            <div >   react homeworks:</div>
-            <Collapsible className={s.Collapse} trigger={'   PUSH ME'} >
-                <div className={s.Links}>
-                    <NavLink to={PATH.PRE_JUNIOR}>PreJunor</NavLink>
-                    <NavLink to={PATH.JUNIOR}>  Junior  </NavLink>
-                    <NavLink to={PATH.JUNIOR_PLUS}>Junior+</NavLink>
+        <HashRouter>
+            <div className={s.App}>
 
+                <div> react homeworks:</div>
+
+                <div className={s.links}>
+                    <div className={s.inner}><NavLink to={PATH.PRE_JUNIOR}>PreJunior</NavLink></div>
+                    <div className={s.inner}><NavLink to={PATH.JUNIOR}>Junior</NavLink></div>
+                    <div className={s.inner}><NavLink to={PATH.JUNIOR_PLUS}>Junior+</NavLink></div>
+                    <div className={s.inner}>Links</div>
                 </div>
-            </Collapsible>
-            {/*<HW1/>*/}
-            {/*<HW2/>*/}
-            {/*<HW3/>*/}
-            {/*<HW4/>*/}
-            <HW5/>
+
+                {/*<HW1/>*/}
+                {/*<HW2/>*/}
+                {/*<HW3/>*/}
+                {/*<HW4/>*/}
+                <HW5/>
 
 
-        </div>
-</HashRouter>
+            </div>
+        </HashRouter>
     )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
